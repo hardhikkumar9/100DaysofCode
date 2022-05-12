01bad1d392142ff9ab717e0bd8597e6e56575c90
Hardhik Kumar <hardhikkumar9@gmail.com>
2022-05-13 02:32:13 +0530

    Update12

---
+++ Day12/src/Arrays.java
@@ -0,0 +1,17 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        //syntax
+        // Datatype[] variable_name = new datatype [size]
+
+        // store 5 roll no.
+        int[] rollno = new int [5];
+
+        //or directly
+        int[] rollno2 = new int []{5, 4, 66, 77, 23};
+        System.out.println(rollno[1]);
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+    }
+}
---
+++ Day12/src/Input.java
@@ -0,0 +1,15 @@
+public class Input {
+    public static void main(String[] args) {
+        int arr[] = new int[6];
+        arr[0] = 33;
+        arr[1] = 64;
+        arr[2] = 39;
+        arr[3] = 13;
+        arr[4] = 24;
+        arr[5] = 332;
+
+        System.out.println(arr[4]);
+
+        //input using for loop
+    }
+}
