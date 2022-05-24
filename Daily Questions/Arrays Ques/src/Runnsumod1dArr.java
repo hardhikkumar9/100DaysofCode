f21c720cfd11d0d268a3833abc3e1d8416a82a33
Hardhik Kumar <hardhikkumar9@gmail.com>
2022-05-24 23:42:19 +0530

    Array 8

---
+++ Daily Questions/Arrays Ques/src/Runnsumod1dArr.java
@@ -0,0 +1,16 @@
+public class Runnsumod1dArr {
+    public static void main(String[] args) {
+        class Solution {
+            public int[] runningSum(int[] nums) {
+                int n = nums.length;
+                int [] arr = new int[n];
+                int sum = 0;
+                for(int i = 0;i < n;i++) {
+                    arr[i] = sum + nums[i];
+                    sum = sum + nums[i];
+                }
+                return arr;
+            }
+        }
+    }
+}
