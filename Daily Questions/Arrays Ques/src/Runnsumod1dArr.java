acc24a09d7184cb0daed163581c2d29962e6f7e5
Hardhik Kumar <hardhikkumar9@gmail.com>
2022-05-25 22:55:42 +0530

    Array change

--- Daily Questions/Arrays Ques/src/Runnsumod1dArr.java
+++ Daily Questions/Arrays Ques/src/Runnsumod1dArr.java
@@ -3,9 +3,9 @@ public class Runnsumod1dArr {
         class Solution {
             public int[] runningSum(int[] nums) {
                 int n = nums.length;
-                int [] arr = new int[n];
+                int[] arr = new int[n];
                 int sum = 0;
-                for(int i = 0;i < n;i++) {
+                for (int i = 0; i < n; i++) {
                     arr[i] = sum + nums[i];
                     sum = sum + nums[i];
                 }
