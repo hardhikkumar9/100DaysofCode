9ffcc5e22172a5dda07ce098a9b005a8cc225620
Hardhik Kumar <hardhikkumar9@gmail.com>
2022-05-31 21:28:25 +0530

    8 change

--- Day12/src/Arrays.java
+++ Day12/src/Arrays.java
@@ -8,8 +8,8 @@ public class Arrays {
         int[] rollno = new int [5];
 
         //or directly
-        int[] rollno2 = new int []{5, 4, 66, 77, 23};
-        System.out.println(rollno2[1]);
+        int[] rollno2 = new int []{5, 4, 65, 77, 23};
+        System.out.println(rollno2[4]);
 
         String[] arr = new String[4];
         System.out.println(arr[0]);
