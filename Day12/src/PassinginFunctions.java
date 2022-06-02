0207c4b40ed0708b001c70d892fab124ec2537b6
Hardhik Kumar <hardhikkumar9@gmail.com>
2022-06-02 23:25:52 +0530

    9 change

--- Day12/src/PassinginFunctions.java
+++ Day12/src/PassinginFunctions.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class PassinginFunctions {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] nums = {1,2,3,4,5};
+        int[] nums = {3,4,5,6,7};
         System.out.println(Arrays.toString(nums));
         change(nums);
         System.out.print(Arrays.toString(nums));
