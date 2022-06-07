3692eaf3a71a0ee3ddcfc8a1152693aae493816b
Hardhik Kumar <hardhikkumar9@gmail.com>
2022-06-07 23:57:21 +0530

    37,38 input

---
+++ Day37/src/Inputs.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for (int j = 0; j < n; j++) {
+            System.out.println(j);
+        }
+    }
+}
