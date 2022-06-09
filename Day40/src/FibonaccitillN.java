6fbc3da8aedccc6406bc92674c960f49466625e0
Hardhik Kumar <hardhikkumar9@gmail.com>
2022-06-09 23:18:26 +0530

    39 till n

--- Day40/src/FibonaccitillN.java
+++ Day40/src/FibonaccitillN.java
@@ -5,6 +5,13 @@ public class FibonaccitillN {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
-
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.println(a);
+            int c = a + b;
+            a = b;
+            b = c;
+        }
     }
 }
