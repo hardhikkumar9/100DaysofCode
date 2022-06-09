b0959949711d86438754f03b75d6911abaad79cd
Hardhik Kumar <hardhikkumar9@gmail.com>
2022-06-09 14:32:06 +0530

    39 till n

--- Day39/src/primetillN.java
+++ Day39/src/primetillN.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class primetillN {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int low = scn.nextInt();
+        int high = scn.nextInt();
 
+        for (int n = low; n <= high ; n++) {
+            int count = 0;
+
+            // try to divide n and increase count
+            for (int div = 2; div * div <= n ; div++) {
+                if (n % div == 0){
+                    count++;
+                    break;
+                }
+            }
+            //
+            if (count == 0) {
+                System.out.println(n);
+            }
+        }
     }
 }
